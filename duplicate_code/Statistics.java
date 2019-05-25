c87f0983ab79ff4912dfe2979edd143843730288
1042998 <crazycat860221@gmail.com>
2019-05-25 14:38:42 +0800

    0609 exercise in ncyu

--- duplicate_code/Statistics.java
+++ duplicate_code/Statistics.java
@@ -4,38 +4,36 @@ public class Statistics
 {
     public double calculateDifferenceOfAverage(double[] array1, double[] array2)
     {
-        double sum1 = 0;
         double average1;
+        average1 = calculateAverage(array1);
 
-        for (double element : array1) {
-            sum1 += element;
-        }
-        average1 = sum1 / array1.length;
-
-        double sum2 = 0;
         double average2;
-
-        for (double element : array2) {
-            sum2 += element;
-        }
-        average2 = sum2 / array2.length;
+        average2 = calculateAverage(array2);
 
         return Math.abs(average1 - average2);
     }
 
     public double calculateSampleVariance(double[] elements)
     {
-        double sum1 = 0;
         double average1;
-        for (double element : elements) {
-            sum1 += element;
-        }
-        average1 = sum1 / elements.length;
+        average1 = calculateAverage(elements);
+
         double temp = 0.0;
         for (double element : elements) {
             temp += Math.pow(element - average1, 2);
         }
         return temp / (elements.length - 1);
+
+    }
+    public double calculateAverage(double[] elements)
+    {
+        double sum = 0;
+        double average;
+        for (double element : elements) {
+            sum += element;
+        }
+        average = sum / elements.length;
+        return average;
 
     }
 }
